e7b61a6050f155ba8411ec8ae013e9ef43798dbb
NeiltonSeguins <seguins.neilton@gmail.com>
2023-12-13 18:28:37 -0300

    Aula 04

--- src/componentes/CampoDigitacao/CampoDigitacao.tsx
+++ src/componentes/CampoDigitacao/CampoDigitacao.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import "./CampoDigitacao.css";
 import MensagemErro from "./MensagemErro";
 
@@ -6,23 +6,27 @@ interface CampoDigitacaoProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   legenda?: string;
   icone?: React.ReactNode;
-  id?: string;
+  id: string;
   erro?: string;
 }
 
-const CampoDigitacao = ({
-  legenda,
-  id,
-  erro,
-  ...rest
-}: CampoDigitacaoProps) => {
-  return (
-    <label htmlFor={id} className="campo__texto--legenda">
-      {legenda}
-      <input className="campo__texto" id={id} {...rest} />
-      {erro && <MensagemErro mensagemErro={erro} />}
-    </label>
-  );
-};
+const CampoDigitacao = forwardRef<HTMLInputElement, CampoDigitacaoProps>(
+  ({ legenda, id, erro, ...rest }, ref) => {
+    const erroId = erro ? `${id}-erro` : undefined;
+    return (
+      <label htmlFor={id} className="campo__texto--legenda">
+        {legenda}
+        <input
+          className="campo__texto"
+          aria-describedby={erroId}
+          id={id}
+          ref={ref}
+          {...rest}
+        />
+        {erro && <MensagemErro id={erroId} mensagemErro={erro} />}
+      </label>
+    );
+  }
+);
 
 export default CampoDigitacao;
--- src/paginas/Home/Inscricao/Inscricao.tsx
+++ src/paginas/Home/Inscricao/Inscricao.tsx
@@ -1,9 +1,41 @@
 import "./Inscricao.css";
-import Botao from "../../../componentes/Botao";
-import CampoDigitacao from "../../../componentes/CampoDigitacao";
-import Tipografia from "../../../componentes/Tipografia";
+import Botao from "@/componentes/Botao";
+import CampoDigitacao from "@/componentes/CampoDigitacao";
+import Tipografia from "@/componentes/Tipografia";
+import { ChangeEvent, FormEvent, useId, useRef, useState } from "react";
 
 const Inscricao = () => {
+  const campoDigitacaoId = useId();
+  const campoDigitacaoRef = useRef<HTMLInputElement | null>(null);
+  const [erro, setErro] = useState<string>("");
+  const [valorCampo, setValorCampo] = useState<string>("");
+
+  const validarCampo = () => {
+    if (campoDigitacaoRef.current) {
+      const valorDoCampo = campoDigitacaoRef.current.value;
+
+      if (valorDoCampo.length < 5) {
+        setErro("O campo deve ter pelo menos 5 caracteres");
+        campoDigitacaoRef.current.focus();
+      } else {
+        setErro("");
+      }
+    }
+  };
+
+  const aoMudarCampoForm = (e: ChangeEvent<HTMLInputElement>) => {
+    setValorCampo(e.target.value);
+    validarCampo();
+  };
+
+  const aoSubmeterForm = (e: FormEvent) => {
+    e.preventDefault();
+    validarCampo();
+    if (!erro) {
+      setValorCampo("");
+    }
+  };
+
   return (
     <section className="secao__inscricao">
       <div className="secao__inscricao--conteudo">
@@ -14,14 +46,25 @@ const Inscricao = () => {
           Cadastre seu email em nossa newsletter e saiba dos descontos, cupons e
           novidades em primeira mão!
         </Tipografia>
-        <form className="secao__inscricao--formulario">
+        <form
+          onSubmit={aoSubmeterForm}
+          noValidate
+          className="secao__inscricao--formulario"
+        >
           <CampoDigitacao
+            ref={campoDigitacaoRef}
+            id={campoDigitacaoId}
             type="email"
-            placeholder="Digite seu melhor endereço de email"
             name="inscricao"
+            placeholder="Digite seu melhor endereço de email"
+            erro={erro}
+            onChange={aoMudarCampoForm}
+            value={valorCampo}
             style={{ width: "650px" }}
           />
-          <Botao variante="primario">Inscrever</Botao>
+          <Botao variante="primario" type="submit">
+            Inscrever
+          </Botao>
         </form>
       </div>
     </section>
